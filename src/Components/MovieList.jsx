86d286ea01e336595d22500c6c2a775b1aef26ba
LauraVert23 <laura.ribeiro@vert-capital.com>
2025-03-19 12:31:02 -0300

    finalizing the front-end of the project

--- src/Components/MovieList.jsx
+++ src/Components/MovieList.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useRef, useState } from "react";
 import { IoChevronBackOutline, IoChevronForwardOutline } from "react-icons/io5";
 import GlobalAPI from "../Services/GlobalAPI";
+import HrMovieCard from "./HrMovieCard";
 import MovieCard from "./MovieCard";
 
-function MovieList({ genreId }) {
+function MovieList({ genreId, index_ }) {
   const [movieList, setMovieList] = useState([]);
   const elementRef = useRef(null);
   useEffect(() => {
@@ -26,8 +27,10 @@ function MovieList({ genreId }) {
     <div className="relative">
       <IoChevronBackOutline
         onClick={() => sliderLeft(elementRef.current)}
-        className="text-[50px] text-white p-2 z-10 cursor-pointer
-        hidden md:block absolute mt-[150px]"
+        className={`text-[50px] text-white p-2 z-10 cursor-pointer
+            hidden md:block absolute ${
+              index_ % 3 == 0 ? "mt-[80px]" : "mt-[150px]"
+            }`}
       />
       <div
         ref={elementRef}
@@ -35,13 +38,19 @@ function MovieList({ genreId }) {
     pt-4 px-3 pb-4"
       >
         {movieList.map((item, index) => (
-          <MovieCard movie={item} />
+          <>
+            {index_ % 3 == 0 ? (
+              <HrMovieCard movie={item} />
+            ) : (
+              <MovieCard movie={item} />
+            )}
+          </>
         ))}
       </div>
       <IoChevronForwardOutline
         onClick={() => sliderRight(elementRef.current)}
-        className="text-[50px] text-white hidden md:block p-2 z-10 
-    top-0 absolute right-0 mt-[150px] cursor-pointer"
+        className={`text-[50px] text-white hidden md:block p-2 cursor-pointer z-10 
+    top-0 absolute right-0 ${index_ % 3 == 0 ? "mt-[80px]" : "mt-[150px]"} `}
       />
     </div>
   );
